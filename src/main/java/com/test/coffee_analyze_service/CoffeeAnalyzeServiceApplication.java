c880d1320fcad724f7e66e4c64e250efde147df4
Bulat <bulatsafiullin2612@gmail.com>
2024-10-14 23:40:15 +0300

    Initial commit

---
+++ src/main/java/com/test/coffee_analyze_service/CoffeeAnalyzeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.test.coffee_analyze_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeeAnalyzeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoffeeAnalyzeServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/coffee_analyze_service/CoffeeAnalyzeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.coffee_analyze_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeAnalyzeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
